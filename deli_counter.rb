01f69bb11d0fdba1dac6b012b69ca9194c7f678f
Phoebe Lau <phoebelau7@gmail.com>
2017-11-28 17:18:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,15 +4,15 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
-  else 
-    
+  else
+
   end
 end
 
 def take_a_number(katz_deli, "name")
   line_position = []
   katz_deli.each_with_index {|customer, position|
-    line_position << "Welcome, #{customer}. You are number #{position+1} in line."
+    line_position << "Welcome, #{name}. You are number #{position+1} in line."
 }
 return line_position
 end
