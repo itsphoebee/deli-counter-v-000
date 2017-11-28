2fde8fb4ae2053eb0784438d70cb735b0294ad0c
Phoebe Lau <phoebelau7@gmail.com>
2017-11-28 18:06:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{take_a_number(katz_deli, index)}"
+    puts "The line is currently: #{take_a_number(katz_deli, )}"
   end
 end
 
