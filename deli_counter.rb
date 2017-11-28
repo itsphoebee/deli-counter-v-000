f11395af440b37e03474cd0620de27cb540ffec6
Phoebe Lau <phoebelau7@gmail.com>
2017-11-28 17:18:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,21 @@
 # Write your code here.
+katz_deli = []
 
 def line(katz_deli)
   if katz_deli.length == 0
-  puts "The line is currently empty."
-end
+    puts "The line is currently empty."
+  else 
+    
+  end
 end
 
-def take_a_number(katz_deli)
+def take_a_number(katz_deli, "name")
   line_position = []
   katz_deli.each_with_index {|customer, position|
     line_position << "Welcome, #{customer}. You are number #{position+1} in line."
 }
 return line_position
+end
+
+def now_serving
 end
