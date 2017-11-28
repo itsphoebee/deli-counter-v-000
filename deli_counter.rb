1aff949122ad0f4755e9ebb95b1bb4b70cc5f384
Phoebe Lau <phoebelau7@gmail.com>
2017-11-28 19:13:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
     katz_deli.each_with_index {|customer, position|
       current_line << "#{position+1}. #{customer} "
     }
-  return "The line is currently" << current_line
+  return "The line is currently" << current_line.join
 end
 
 def take_a_number(katz_deli, name)
