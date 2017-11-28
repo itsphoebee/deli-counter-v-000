b25bf7fbae2fc010de0d709f816dec0ab6575548
Phoebe Lau <phoebelau7@gmail.com>
2017-11-28 17:55:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def take_a_number(katz_deli, name)
     puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
   katz_deli.each_with_index {|customer, position|
     line_position << "Welcome, #{customer}. You are number #{position+1} in line."
-}
+  }
 return line_position
 end
 
@@ -26,5 +26,6 @@ def now_serving(katz_deli)
     puts "There is nobody waiting to be served!"
   else
   puts "Currently serving #{katz_deli[0]}."
+  katz_deli.shift
 end
 end
