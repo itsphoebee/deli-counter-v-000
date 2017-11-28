bc7dc457138b718a2af65c4525a31ddfdbf5a13a
Phoebe Lau <phoebelau7@gmail.com>
2017-11-28 18:00:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,8 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-
-
+    now_serving(katz_deli)
+    puts "The line is currently: "
   end
 end
 
@@ -25,7 +25,7 @@ def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
-  puts "Currently serving #{katz_deli[0]}."
+    puts "Currently serving #{katz_deli[0]}."
   katz_deli.shift
   end
 end
