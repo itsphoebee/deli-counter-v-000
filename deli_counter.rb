64c7cf75c646385c093510ce5f84fa9060d5390b
Phoebe Lau <phoebelau7@gmail.com>
2017-11-28 18:28:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,7 +24,10 @@ end
 
 
 def now_serving(katz_deli)
-  katz_deli.length == 0 ? puts "There is nobody waiting to be served!" : puts "Currently serving #{katz_deli[0]}."
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
   katz_deli.shift
   end
 end
