8c5428da015f18d1418cc1f525344b8c8c5e561f
Phoebe Lau <phoebelau7@gmail.com>
2017-11-28 19:20:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,9 @@ def line(katz_deli)
   else
     current_line=[]
     katz_deli.each_with_index {|customer, position|
-      current_line << "#{position+1}. #{customer} "
+      current_line << " #{position+1}. #{customer}"
     }
-    puts "The line is currently: "<<(current_line).join
+    puts "The line is currently:"<<(current_line).join
   end
 end
 
