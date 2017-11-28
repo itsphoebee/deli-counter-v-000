1b4b80f6fd3adb4660810312a467df9d1de94d9a
Phoebe Lau <phoebelau7@gmail.com>
2017-11-28 19:01:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,7 @@
 # Write your code here.
 def line(katz_deli)
   if katz_deli.length > 0
+    puts "The line is currently: "
     katz_deli.each_with_index {|customer, position|
       puts "#{position+1}. #{customer} "
     }
