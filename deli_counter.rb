144251b33513d0b8e6d9e9ccbab66a385d6a9f72
Phoebe Lau <phoebelau7@gmail.com>
2017-11-28 17:04:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,10 @@
 # Write your code here.
 
-def line[katz_deli]
 
-end
 
 def take_a_number(katz_deli, "name")
   katz_deli.each_with_index {|customer, position|
-    katz_deli[customer] = position  
+    katz_deli[customer] = position
   return "Welcome, #{customer}. You are number #{position} in line."
 }
 end
