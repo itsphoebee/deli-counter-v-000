7dd28a7e53e0e6c3e9b3c737b0cd4d0707321a34
Phoebe Lau <phoebelau7@gmail.com>
2017-11-28 18:47:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,10 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: " << "#{katz_deli.each_with_index {|customers, number|
+    katz_deli.each_with_index {|customers, number|
       puts number + 1
       puts customers
     }
-  }"
-    #{} .zip(katz_deli)}"
   end
 end
 
